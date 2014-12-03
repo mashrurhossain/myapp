ac1c6ef1aadd2bf134879ca2adbbeaad2706d88b
Mashrur Hossain <mashrur.hossain@gmail.com>
2014-12-03 19:11:44 +0000

    Add many to many relationship with group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203183003) do
+ActiveRecord::Schema.define(version: 20141203190556) do
+
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id"
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
 
   create_table "posts", force: true do |t|
     t.text     "content"
